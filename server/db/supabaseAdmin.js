7b6b8be8c7925409987703a8f3129b207edf07e2
Gabriel Baroni <gdepaulabaroni@gmail.com>
2025-11-12 14:14:44 -0300

    Adicionando comentários no arquivo supabaseAdmin.js

--- server/db/supabaseAdmin.js
+++ server/db/supabaseAdmin.js
@@ -1,16 +1,19 @@
+//Importações necessárias
 import { createClient } from '@supabase/supabase-js';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
+// Configuração do Supabase usando variáveis de ambiente
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_KEY;
 
-
+// Verificação se as variáveis de ambiente estão definidas
 if (!supabaseUrl || !supabaseKey) {
     throw new Error('Supabase URL e/ou chave de API não encontrados no arquivo .env');
   }
 
+// Criação do cliente Supabase
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
